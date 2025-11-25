d168f3f3309c240a7e6d117122b703d854ba59b4
Gregor Stocks <102307+GregorStocks@users.noreply.github.com>
2025-11-24 21:39:25 -0800

    un-ignore gen client

---
+++ ramekin-ui/generated-client/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./ErrorResponse";
+export * from "./LoginRequest";
+export * from "./LoginResponse";
+export * from "./PingResponse";
+export * from "./SignupRequest";
+export * from "./SignupResponse";
+export * from "./UnauthedPingResponse";
+export * from "./UploadPhotoResponse";
