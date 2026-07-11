63c9b3e03e153e84b93062670e2abe38d73cf40d
Gregor Stocks <102307+GregorStocks@users.noreply.github.com>
2026-07-11 14:10:13 -0700

    Define ingredient vector coding keys

--- ramekin-ios/RamekinTests/IngredientFormattingSharedVectorTests.swift
+++ ramekin-ios/RamekinTests/IngredientFormattingSharedVectorTests.swift
@@ -1,35 +1,44 @@
 import XCTest
 @testable import Ramekin
 
+private struct IngredientFormattingVector: Decodable {
+    let name: String
+    let ingredient: Ingredient
+    let options: IngredientFormattingOptions
+    let expected: String
+}
+
+private struct IngredientFormattingOptions: Decodable {
+    let scale: Double?
+    let includeAlternatives: Bool
+    let includeNote: Bool
+
+    private enum CodingKeys: String, CodingKey {
+        case scale
+        case includeAlternatives
+        case includeNote
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        scale = try container.decodeIfPresent(Double.self, forKey: .scale)
+        includeAlternatives = try container.decodeIfPresent(
+            Bool.self,
+            forKey: .includeAlternatives
+        ) ?? false
+        includeNote = try container.decodeIfPresent(Bool.self, forKey: .includeNote) ?? false
+    }
+}
+
 final class IngredientFormattingSharedVectorTests: XCTestCase {
-    private struct Vector: Decodable {
-        let name: String
-        let ingredient: Ingredient
-        let options: Options
-        let expected: String
-    }
-
-    private struct Options: Decodable {
-        let scale: Double?
-        let includeAlternatives: Bool
-        let includeNote: Bool
-
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            scale = try container.decodeIfPresent(Double.self, forKey: .scale)
-            includeAlternatives = try container.decodeIfPresent(
-                Bool.self,
-                forKey: .includeAlternatives
-            ) ?? false
-            includeNote = try container.decodeIfPresent(Bool.self, forKey: .includeNote) ?? false
-        }
-    }
-
     func testIngredientFormattingMatchesSharedVectors() throws {
         let url = try XCTUnwrap(
             Bundle(for: Self.self).url(forResource: "ingredient-formatting", withExtension: "json")
         )
-        let vectors = try JSONDecoder().decode([Vector].self, from: Data(contentsOf: url))
+        let vectors = try JSONDecoder().decode(
+            [IngredientFormattingVector].self,
+            from: Data(contentsOf: url)
+        )
 
         for vector in vectors {
             XCTAssertEqual(
